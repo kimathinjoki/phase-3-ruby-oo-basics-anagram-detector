ab1075a47746153022829eb66b8d2b3e1d34aa0d
kimathinjoki <dfkimathinjoki@gmail.com>
2023-02-08 10:36:49 -0800

    to be completed

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,21 @@
 # Your code goes here!
+class Anagram
+
+    attr_reader :initial_word
+
+    def initialize(initial_word)
+        @initial_word = initial_word
+    end
+
+    def match(arr)
+        arr.map do |b|
+            compare_word = b.chars
+            input_word = @initial_word.chars
+            # puts input_word
+            if compare_word.sort == input_word.sort
+                compare_word.join("")
+            end
+        end
+    end
+
+end
